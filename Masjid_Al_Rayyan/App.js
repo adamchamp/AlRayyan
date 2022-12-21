ac8352550ee00ca43d2ee2d4c279ca48f8d69c99
Iqbal-Talha <tiqbal02@my.yorku.ca>
2022-12-21 10:57:43 -0500

    components setup

--- Masjid_Al_Rayyan/App.js
+++ Masjid_Al_Rayyan/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App()
+{
 	return (
 			<View style={styles.container}>
 				<Text>Text here</Text>
+
+
 				<StatusBar style="auto" />
 			</View>
 			);
 }
+
 
 const styles = StyleSheet.create({
 	container: {
