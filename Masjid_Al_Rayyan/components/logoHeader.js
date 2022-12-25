4a2a3757a7da5bfa9a90cf59e416274741178c47
Iqbal-Talha <tiqbal02@my.yorku.ca>
2022-12-25 10:54:37 -0500

    images, css

--- Masjid_Al_Rayyan/components/logoHeader.js
+++ Masjid_Al_Rayyan/components/logoHeader.js
@@ -4,25 +4,8 @@ import styled from "styled-components";
 export const logoHeader = ({}) => {
 	return (
 			<logoHeader>
-				<LogoIcon src={`https://file.rendit.io/n/ioXSkSz4xZcrkzToaTyS.png`} />
-				<logoName src={`https://file.rendit.io/n/peztbS3MnMVwZPVg5o7a.png`} />
+				<LogoIcon src={'../assets/logoImage.png'} />
+				<logoName src={'../assets/logoText.png'} />
 			</logoHeader>
 			);
 };
-
-const logoHeader = styled.div`
-  width: 264px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 45px 0px 20px 0px;
-`;
-const LogoIcon = styled.img`
-  width: 49px;
-  height: 47px;
-`;
-const logoName = styled.img`
-  width: 155px;
-  height: 47px;
-`;
