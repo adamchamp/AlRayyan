2a9e7f2ebb9120d1c7e2825527756390c0426e23
Iqbal-Talha <tiqbal02@my.yorku.ca>
2023-01-15 13:43:33 -0500

    donate Example

--- screens/Donate.js
+++ screens/Donate.js
@@ -1,24 +1,30 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {colors} from '../config/theme';
+import {LogoHeader} from '../components/LogoHeader';
+import {DonateCard} from '../components/DonateCard';
 
 const Donate = () => {
 	const theme = {mode: "light"};
 	let activeColors = colors[theme.mode];
 
 	return (
-			<View
-				style={{
-								flex: 1,
-								justifyContent: 'center',
-								alignItems: 'center',
-								backgroundColor: activeColors.white
-							}}>
-				<Text>Donate!</Text>
+			<View>
+				<LogoHeader/>
+
+				<DonateCard/>
+				<DonateCard/>
+
 			</View>
 			);
 };
 
 export default Donate;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container:
+			{
+
+			}
+
+});
